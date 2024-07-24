ce701aafc07119f192cd6ec42cb93bb6915ea026
Alice Boone <alicegbone@gmail.com>
2024-07-24 14:41:11 -0700

    Integrate Ai

---
+++ src/main/java/com/capstone/mylittlelibrarybackend/openai/Message.java
@@ -0,0 +1,15 @@
+package com.capstone.mylittlelibrarybackend.openai;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String role;
+    private String content;
+
+}
