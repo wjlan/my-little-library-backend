fd9cb4785190edd3dc8ca658aa8de667f6d80d4a
Alice Boone <alicegbone@gmail.com>
2024-07-30 10:31:12 -0700

    add heroku url

--- src/main/java/com/capstone/mylittlelibrarybackend/config/WebMvcConfig.java
+++ src/main/java/com/capstone/mylittlelibrarybackend/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://my-little-library-backend-841473d33266.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
